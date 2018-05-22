73b4518423ab0fa1ab84504bac11e45cb9a22599
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-05-22 09:29:15 +0300

    Configure loggin

--- dashboard/index.php
+++ dashboard/index.php
@@ -17,8 +17,17 @@ $app = new \Slim\App(['settings' => $config]);
 
 $container = $app->getContainer();
 
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
 $app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("Hello");
+
+     $this->logger->addInfo('Something interesting happened');
 
     return $response;
 });
