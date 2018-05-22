260e3508724c584d6b400dcd331c65b80680b9f2
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-05-22 09:43:24 +0300

    Setup dotenv

--- dashboard/index.php
+++ dashboard/index.php
@@ -5,13 +5,16 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
-$config['db']['host']   = 'localhost';
-$config['db']['user']   = 'user';
-$config['db']['pass']   = 'password';
-$config['db']['dbname'] = 'exampleapp';
+$config['db']['host']   = $_ENV['DB_HOST'];
+$config['db']['user']   = $_ENV['DB_USERNAME'];
+$config['db']['pass']   = $_ENV['DB_PASSWORD'];
+$config['db']['dbname'] = $_ENV['DB_DATABASE'];
 
 $app = new \Slim\App(['settings' => $config]);
 
@@ -22,6 +25,15 @@ $container['logger'] = function($c) {
     $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
     $logger->pushHandler($file_handler);
     return $logger;
+};
+
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
 };
 
 $app->get('/', function (Request $request, Response $response) {
