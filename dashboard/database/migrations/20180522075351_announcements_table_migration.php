ec8932d5999ea98574b2f280e077d62d0f19ef2b
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-05-22 11:01:27 +0300

    Create Announcement migration and seeder

---
+++ dashboard/database/migrations/20180522075351_announcements_table_migration.php
@@ -0,0 +1,22 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class AnnouncementsTableMigration extends AbstractMigration
+{
+    public function up()
+    {
+        $users = $this->table('announcements');
+        $users->addColumn('title', 'string')
+              ->addColumn('path', 'string')
+              ->addColumn('created_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+              ->addColumn('updated_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+              ->save();
+    }
+
+    public function down()
+    {
+        $this->dropTable('announcements');
+    }
+}
---
+++ dashboard/database/seeds/AnnouncementsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class AnnouncementsTableSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+         $faker = Faker\Factory::create();
+         $data = [];
+         for ($i = 0; $i < 5; $i++) {
+            $data[] = [
+                'title'      => $faker->sentence,
+                'path'         => $faker->file('/tmp', '/files'),
+            ];
+         }
+         $this->insert('users', $data);
+    }
+}
